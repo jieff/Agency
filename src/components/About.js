6c6fb57d9ffc082e3832d0acb0761dca870da0cc
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-12-28 10:42:13 -0300

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            {/* Olá, eu sou Jieff
+            <br className="hidden lg:inline-block" />  */}
+            Transformando Ideias em Experiências Digitais Inovadoras
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          Nossa agência é o lugar onde as visões se tornam tangíveis. Transformamos o abstracto em realidade digital. Seja uma ideia para um novo aplicativo, um conceito para uma plataforma web ou uma visão para uma experiência interativa, estamos aqui para fazer essa jornada de transformação acontecer</p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+            Fale Conosco
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Nossos trabalhos
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "O profissionalismo e a dedicação da equipe são verdadeiramente impressionantes. Encontrar uma empresa que não apenas entenda nossas necessidades, mas também as supere, é raro. Eles não apenas entregam, mas excedem as expectativas, proporcionando soluções inovadoras e de alta qualidade. Estou extremamente satisfeito com o serviço prestado.",
+    image: "https://avatars.githubusercontent.com/u/94200054?v=4",
+    name: "Bezalel Miranda",
+    company: "Realy Ksys",
+  },
+  {
+    quote:
+      "A experiência foi excepcional. A equipe não só demonstrou expertise técnica, mas também um compromisso genuíno em entender as demandas específicas do nosso projeto. Sua abordagem minuciosa e soluções personalizadas fizeram toda a diferença. Recomendo-os sem hesitação para qualquer projeto digital complexo",
+    image: "https://avatars.githubusercontent.com/u/93042448?v=4",
+    name: "ALLAN FELIZZETI",
+    company: "Helpdesk Ksys",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
